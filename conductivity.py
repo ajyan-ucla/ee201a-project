aefc3b4d9d82d64f1a1afc0d2527ba199824440d
ajyan-ucla <ajyan@g.ucla.edu>
2026-03-10 07:18:22 -0700

    Create conductivity.py with material data
    Added thermal conductivity values for various materials.

---
+++ conductivity.py
@@ -0,0 +1,23 @@
+"""
+Thermal conductivity values for common materials
+Units: W/(m*K)
+"""
+
+conductivity_values = {
+    "Air": 0.025,
+    "FR-4": 0.1,
+    "Cu-Foil": 400,
+    "Si": 105,
+    "Aluminium": 205,
+    "TIM001": 100,
+    "Glass": 1.36,
+    "TIM": 100,
+    "SnPb 67/37": 36,
+    "Epoxy, Silver filled": 1.6,
+    "SiO2": 1.1,
+    "AlN": 237,
+    "EpAg": 1.6,
+    "Infill_material": 19,
+    "Polymer1": 675,
+    "TIM0p5": 1.0,
+}
